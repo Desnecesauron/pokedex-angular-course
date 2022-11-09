967f4084a66783a0793288b302ccab97bef84140
Desnecesauron <cesinhasampa63@gmail.com>
2022-11-09 11:51:57 -0300

    Hands on 7

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'poke-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
-  constructor() {}
+  constructor(private pokeApiService: PokeApiService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.pokeApiService.apiListAllPokemons.subscribe((res) => res);
+  }
 }
