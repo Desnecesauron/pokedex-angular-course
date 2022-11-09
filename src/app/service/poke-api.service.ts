e96952da2727e4c60bd774debc1d7eb05d81749a
Desnecesauron <cesinhasampa63@gmail.com>
2022-11-09 13:33:36 -0300

    Hands on 8

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -15,8 +15,17 @@ export class PokeApiService {
     return this.http.get<any>(this.url).pipe(
       tap((res) => res),
       tap((res) => {
-        console.log(res);
+        // console.log(res);
+        res.results.map((resPokemons: any) => {
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            (res) => (resPokemons.status = res)
+          );
+        });
       })
     );
+  }
+
+  public apiGetPokemons(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(map((res) => res));
   }
 }
