de5dab8daa89e2776212c7c0399b6c03f751d3ed
Desnecesauron <cesinhasampa63@gmail.com>
2022-11-09 09:57:34 -0300

    Hands on 4

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 //Module Routing
 import { RoutingModule } from './routing.module';
 
+//Module
+import { SharedModule } from '../shared/shared.module';
+
 //Pages
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [HomeComponent, DetailsComponent],
-  imports: [CommonModule, RoutingModule],
+  imports: [CommonModule, RoutingModule, SharedModule],
 })
 export class PagesModule {}
